80cd98661cdba82e55ccf4811cfc29986557d8e2
teraboxneed-design <teraboxneed@gmail.com>
2026-06-25 12:19:29 +0530

    Add custom_visa to attachment-clear confirmation tracker fields

--- sandrose/public/js/employee.js
+++ sandrose/public/js/employee.js
@@ -8,7 +8,7 @@ frappe.ui.form.on("Employee", {
             'custom_certificate_attach', 'custom_data_flow_attach', 'custom_prometric_attach',
             'custom_cv_attach', 'custom_police_clearance_attach', 'custom_acls_attach',
             'custom_noc_attach', 'custom_offer_letter_attach', 'custom_civil_id_attach',
-            'custom_mol_contract_attach', 'custom_license_attach'
+            'custom_mol_contract_attach', 'custom_license_attach', 'custom_visa'
         ];
 
         var original_clear = frappe.ui.form.ControlAttach.prototype.clear_attachment;
